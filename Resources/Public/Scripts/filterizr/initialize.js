d00c49a296cf8f3e8cdb4cd37e0aa540752db945
Markus Dübbert <info@markusduebbert.de>
2023-07-07 17:41:26 +0100

    add files

---
+++ Resources/Public/Scripts/filterizr/initialize.js
@@ -0,0 +1,25 @@
+$(function() {
+    //Simple filter controls
+    $('.simplefilter li').click(function() {
+        $('.simplefilter li').removeClass('active');
+        $(this).addClass('active');
+    });
+    //Multifilter controls
+    $('.multifilter li').click(function() {
+        $(this).toggleClass('active');
+    });
+    //Shuffle control
+    $('.shuffle-btn').click(function() {
+        $('.sort-btn').removeClass('active');
+    });
+    //Sort controls
+    $('.sort-btn').click(function() {
+        $('.sort-btn').removeClass('active');
+        $(this).addClass('active');
+    });
+});
+
+$(function() {
+    //Initialize filterizr with default options
+    $('.filtr-container').filterizr();
+});
